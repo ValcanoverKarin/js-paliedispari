517ba4bfc1b70e4769aa421a4e6b91a2280f7d6a
karin valcanover <valcanoverkarin@outlook.it>
2021-03-29 23:02:03 +0200

    chiedo parola

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,2 +1,8 @@
 //Chiedere all’utente di inserire una parola 
 //Creare una funzione per capire se la parola inserita è palindroma
+
+//chiedere una parola all' utente (stringa)
+var parolaUtente = prompt('inserisci una parola');
+console.log(parolaUtente);
+
+
