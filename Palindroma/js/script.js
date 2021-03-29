0cfa0b96e2b4fb7b2e932b9ef5e3a3041895a50a
karin valcanover <valcanoverkarin@outlook.it>
2021-03-29 23:17:10 +0200

    funzione per vedere se la parola è palindroma o no

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -7,9 +7,30 @@ console.log(parolaUtente);
 
 //creo la funzione parola palindroma o non palindroma
 function valutaParola (parolaDaValutare){
-    //crea ciclo per leggere la parola che inserisce l utente da sinistra a destra
+//creo ciclo per leggere la parola che inserisce l utente da sinistra a destra
 for (var i = 0; i < parolaUtente.length; i++){
     var parolaDritta = parolaUtente[i];
     console.log(parolaDritta);
 }
+//creo ciclo per leggere la parola che inserisce l utente da destra a sinistra
+for (var i = parolaUtente.length; i >= 0; i--){
+    var parolaContraria = parolaUtente[i];
+    console.log(parolaContraria);
+}
+    var risultato;
+
+//se la parola letta dritta o al contrario è uguale è palindroma se no no
+if(parolaDritta == parolaContraria){
+    risultato = 'parola palindroma';
+} else {
+    risultato = 'parola non palindroma';
+}
+return risultato;
+}
+
+var parolaValutata = valutaParola(parolaUtente);
+console.log(parolaValutata);
+
+//stampa
+alert(parolaValutata);
 
