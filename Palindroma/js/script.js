b426bdb89c50e0e88825693400ae7a53b83758d8
karin valcanover <valcanoverkarin@outlook.it>
2021-03-29 23:04:14 +0200

    funzione parola palindroma o no

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -5,4 +5,11 @@
 var parolaUtente = prompt('inserisci una parola');
 console.log(parolaUtente);
 
+//creo la funzione parola palindroma o non palindroma
+function valutaParola (parolaDaValutare){
+    //crea ciclo per leggere la parola che inserisce l utente da sinistra a destra
+for (var i = 0; i < parolaUtente.length; i++){
+    var parolaDritta = parolaUtente[i];
+    console.log(parolaDritta);
+}
 
